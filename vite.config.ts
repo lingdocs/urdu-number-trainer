464790fa9366a725b02085eaffe307a47a493936
adueck <adueck@users.noreply.github.com>
2024-12-17 11:22:10 -0500

    update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,25 @@ export default defineConfig({
       // add this to cache all the
       // static assets in the public folder
       includeAssets: ["**/*"],
+      manifest: {
+        name: "Urdu Number Trainer",
+        short_name: "Number Trainer",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
     }),
   ],
 });
