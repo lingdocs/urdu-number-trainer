bc66d3592e289e74ef087fe41dc84637605160a7
adueck <adueck@users.noreply.github.com>
2024-12-21 22:10:18 -0500

    add sw

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   workbox: {
-    //     globPatterns: ["**/*"],
-    //   },
-    //   // add this to cache all the
-    //   // static assets in the public folder
-    //   includeAssets: ["**/*"],
-    //   manifest: {
-    //     name: "Urdu Number Trainer",
-    //     short_name: "Number Trainer",
-    //     icons: [
-    //       {
-    //         src: "/android-chrome-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/android-chrome-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     theme_color: "#ffffff",
-    //     background_color: "#ffffff",
-    //     display: "standalone",
-    //   },
-    // }),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "Urdu Number Trainer",
+        short_name: "Number Trainer",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    }),
   ],
 });
