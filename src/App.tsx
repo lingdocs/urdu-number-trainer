7e2ddfd649a87e0bbd80f2a51f2444d4e8344461
adueck <adueck@users.noreply.github.com>
2024-12-21 23:44:08 -0500

    safe state change

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   const [state, dispatch] = useStickyReducer(
     reducer(reward),
     initialState,
-    "numbers-state-1"
+    "numbers-state-2"
   );
   const progress = getProgress(state);
   function rollDice() {
